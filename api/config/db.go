40e820398294e31d513df7fd8fe27e0ed4bbc1de
Ismail Becit <37474154+ismailbecit@users.noreply.github.com>
2022-11-01 18:36:09 +0300

    Update db.go

--- api/config/db.go
+++ api/config/db.go
@@ -10,7 +10,7 @@ import (
 var Database *gorm.DB
 
 func Conn() *gorm.DB {
-	dsn := "root:aea76026@tcp(127.0.0.1:3306)/carsell?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:şifre@tcp(127.0.0.1:3306)/carsell?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Veri Tabanına Bağlanılamadı")
